a51b080b0d340a024d23851d81fa6d2ab01b8a82
shervin-glitch <shervin2234@gmail.com>
2022-05-17 20:24:21 -0700

    Commited

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 try:
     import os
     import sys
+    import asyncio
+    import platform
     import subprocess
     from typing import Any
 
@@ -11,13 +13,27 @@ finally:
     ...
     
     
-    
-class PythonLibraryUpdator:
+
+class PythonVersionUpdator():
     def __init__(self) -> Any:
-        self.currentPythonVersion : int = int(sys.version[0])
+        super(PythonVersionUpdator , self).__init__()
+        if (platform.system()[0].upper() in ['W' , 'L']):
+            try:
+                subprocess.call(args=['py' , '-m' , 'pip' , 'install' , '--upgrade' , 'pip'])
+            except:
+                subprocess.call(args=['python3' , '-m' , 'pip' , 'install' , '--upgrade' , 'pip'])
+
+    
+    
+    
+class PythonLibraryUpdator():
+    
+    @classmethod
+    async def update(cls) -> Any:
+        cls.currentPythonVersion : int = int(sys.version[0])
         
-        if (self.currentPythonVersion < 3):
-            print(f'Your Current Python Version is {sys.version} , Update Your Python to Version 3')
+        if (cls.currentPythonVersion < 3):
+            print(await f'Your Current Python Version is {sys.version} , Update Your Python to Version 3')
         else:
             for package in os.popen(cmd='pip freeze').readlines():
                 subprocess.call(args=['pip3' , 'install' , '--upgrade' , package.strip().split(sep='==')[0]])
@@ -27,4 +43,5 @@ class PythonLibraryUpdator:
             
 
 if (__name__ == '__main__' and __package__ is None):
-    PythonLibraryUpdator()
+    PythonVersionUpdator()
+    asyncio.run(PythonLibraryUpdator().update())
